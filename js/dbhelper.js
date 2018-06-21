da4d6006da751a0d05627fa96ce029f65576454f
Mikko Rinne <mikko.rinne@iki.fi>
2018-06-21 18:47:20 +0300

    feat: Download from server, save to IDB
    Removed the old restaurants.json-file. Changed DBHelper DATABASE_URL
    to point to the sail server on localhost / port 1337. Modified
    fetchRestaurants and fetchRestaurantById to work based on the
    (slightly simpler) format returned by the server. Embedded the
    idb.js library interface code to dbhelper.js. Created the IDB
    calls to default to loading the requested resource from the db.
    If not available, the resource is fetched from the server and
    saved to the db. Created also an additional keyval objectstore
    to the same db the save the status of whether the full set of
    restaurants has been fetched. Made minor modifications to
    imageUrlForRestaurant and smallImageUrlForRestaurant to comply
    with the changes in naming compared to the restaurants.json file.
    The previous version also had figure captions for alt text added
    by me. These were not available in the sail database, so changed
    altTextForRestaurantImage to serve the boring restaurant.name
    instead.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,6 +1,333 @@
-/**
+/**********************
+ * idb.js
+ *
+ * By Jake Archibald
+ * https://github.com/jakearchibald/idb/blob/master/lib/idb.js
+ **********************/
+
+'use strict';
+
+(function() {
+  function toArray(arr) {
+    return Array.prototype.slice.call(arr);
+  }
+
+  function promisifyRequest(request) {
+    return new Promise(function(resolve, reject) {
+      request.onsuccess = function() {
+        resolve(request.result);
+      };
+
+      request.onerror = function() {
+        reject(request.error);
+      };
+    });
+  }
+
+  function promisifyRequestCall(obj, method, args) {
+    var request;
+    var p = new Promise(function(resolve, reject) {
+      request = obj[method].apply(obj, args);
+      promisifyRequest(request).then(resolve, reject);
+    });
+
+    p.request = request;
+    return p;
+  }
+
+  function promisifyCursorRequestCall(obj, method, args) {
+    var p = promisifyRequestCall(obj, method, args);
+    return p.then(function(value) {
+      if (!value) return;
+      return new Cursor(value, p.request);
+    });
+  }
+
+  function proxyProperties(ProxyClass, targetProp, properties) {
+    properties.forEach(function(prop) {
+      Object.defineProperty(ProxyClass.prototype, prop, {
+        get: function() {
+          return this[targetProp][prop];
+        },
+        set: function(val) {
+          this[targetProp][prop] = val;
+        }
+      });
+    });
+  }
+
+  function proxyRequestMethods(ProxyClass, targetProp, Constructor, properties) {
+    properties.forEach(function(prop) {
+      if (!(prop in Constructor.prototype)) return;
+      ProxyClass.prototype[prop] = function() {
+        return promisifyRequestCall(this[targetProp], prop, arguments);
+      };
+    });
+  }
+
+  function proxyMethods(ProxyClass, targetProp, Constructor, properties) {
+    properties.forEach(function(prop) {
+      if (!(prop in Constructor.prototype)) return;
+      ProxyClass.prototype[prop] = function() {
+        return this[targetProp][prop].apply(this[targetProp], arguments);
+      };
+    });
+  }
+
+  function proxyCursorRequestMethods(ProxyClass, targetProp, Constructor, properties) {
+    properties.forEach(function(prop) {
+      if (!(prop in Constructor.prototype)) return;
+      ProxyClass.prototype[prop] = function() {
+        return promisifyCursorRequestCall(this[targetProp], prop, arguments);
+      };
+    });
+  }
+
+  function Index(index) {
+    this._index = index;
+  }
+
+  proxyProperties(Index, '_index', [
+    'name',
+    'keyPath',
+    'multiEntry',
+    'unique'
+  ]);
+
+  proxyRequestMethods(Index, '_index', IDBIndex, [
+    'get',
+    'getKey',
+    'getAll',
+    'getAllKeys',
+    'count'
+  ]);
+
+  proxyCursorRequestMethods(Index, '_index', IDBIndex, [
+    'openCursor',
+    'openKeyCursor'
+  ]);
+
+  function Cursor(cursor, request) {
+    this._cursor = cursor;
+    this._request = request;
+  }
+
+  proxyProperties(Cursor, '_cursor', [
+    'direction',
+    'key',
+    'primaryKey',
+    'value'
+  ]);
+
+  proxyRequestMethods(Cursor, '_cursor', IDBCursor, [
+    'update',
+    'delete'
+  ]);
+
+  // proxy 'next' methods
+  ['advance', 'continue', 'continuePrimaryKey'].forEach(function(methodName) {
+    if (!(methodName in IDBCursor.prototype)) return;
+    Cursor.prototype[methodName] = function() {
+      var cursor = this;
+      var args = arguments;
+      return Promise.resolve().then(function() {
+        cursor._cursor[methodName].apply(cursor._cursor, args);
+        return promisifyRequest(cursor._request).then(function(value) {
+          if (!value) return;
+          return new Cursor(value, cursor._request);
+        });
+      });
+    };
+  });
+
+  function ObjectStore(store) {
+    this._store = store;
+  }
+
+  ObjectStore.prototype.createIndex = function() {
+    return new Index(this._store.createIndex.apply(this._store, arguments));
+  };
+
+  ObjectStore.prototype.index = function() {
+    return new Index(this._store.index.apply(this._store, arguments));
+  };
+
+  proxyProperties(ObjectStore, '_store', [
+    'name',
+    'keyPath',
+    'indexNames',
+    'autoIncrement'
+  ]);
+
+  proxyRequestMethods(ObjectStore, '_store', IDBObjectStore, [
+    'put',
+    'add',
+    'delete',
+    'clear',
+    'get',
+    'getAll',
+    'getKey',
+    'getAllKeys',
+    'count'
+  ]);
+
+  proxyCursorRequestMethods(ObjectStore, '_store', IDBObjectStore, [
+    'openCursor',
+    'openKeyCursor'
+  ]);
+
+  proxyMethods(ObjectStore, '_store', IDBObjectStore, [
+    'deleteIndex'
+  ]);
+
+  function Transaction(idbTransaction) {
+    this._tx = idbTransaction;
+    this.complete = new Promise(function(resolve, reject) {
+      idbTransaction.oncomplete = function() {
+        resolve();
+      };
+      idbTransaction.onerror = function() {
+        reject(idbTransaction.error);
+      };
+      idbTransaction.onabort = function() {
+        reject(idbTransaction.error);
+      };
+    });
+  }
+
+  Transaction.prototype.objectStore = function() {
+    return new ObjectStore(this._tx.objectStore.apply(this._tx, arguments));
+  };
+
+  proxyProperties(Transaction, '_tx', [
+    'objectStoreNames',
+    'mode'
+  ]);
+
+  proxyMethods(Transaction, '_tx', IDBTransaction, [
+    'abort'
+  ]);
+
+  function UpgradeDB(db, oldVersion, transaction) {
+    this._db = db;
+    this.oldVersion = oldVersion;
+    this.transaction = new Transaction(transaction);
+  }
+
+  UpgradeDB.prototype.createObjectStore = function() {
+    return new ObjectStore(this._db.createObjectStore.apply(this._db, arguments));
+  };
+
+  proxyProperties(UpgradeDB, '_db', [
+    'name',
+    'version',
+    'objectStoreNames'
+  ]);
+
+  proxyMethods(UpgradeDB, '_db', IDBDatabase, [
+    'deleteObjectStore',
+    'close'
+  ]);
+
+  function DB(db) {
+    this._db = db;
+  }
+
+  DB.prototype.transaction = function() {
+    return new Transaction(this._db.transaction.apply(this._db, arguments));
+  };
+
+  proxyProperties(DB, '_db', [
+    'name',
+    'version',
+    'objectStoreNames'
+  ]);
+
+  proxyMethods(DB, '_db', IDBDatabase, [
+    'close'
+  ]);
+
+  // Add cursor iterators
+  // TODO: remove this once browsers do the right thing with promises
+  ['openCursor', 'openKeyCursor'].forEach(function(funcName) {
+    [ObjectStore, Index].forEach(function(Constructor) {
+      Constructor.prototype[funcName.replace('open', 'iterate')] = function() {
+        var args = toArray(arguments);
+        var callback = args[args.length - 1];
+        var nativeObject = this._store || this._index;
+        var request = nativeObject[funcName].apply(nativeObject, args.slice(0, -1));
+        request.onsuccess = function() {
+          callback(request.result);
+        };
+      };
+    });
+  });
+
+  // polyfill getAll
+  [Index, ObjectStore].forEach(function(Constructor) {
+    if (Constructor.prototype.getAll) return;
+    Constructor.prototype.getAll = function(query, count) {
+      var instance = this;
+      var items = [];
+
+      return new Promise(function(resolve) {
+        instance.iterateCursor(query, function(cursor) {
+          if (!cursor) {
+            resolve(items);
+            return;
+          }
+          items.push(cursor.value);
+
+          if (count !== undefined && items.length == count) {
+            resolve(items);
+            return;
+          }
+          cursor.continue();
+        });
+      });
+    };
+  });
+
+  var exp = {
+    open: function(name, version, upgradeCallback) {
+      var p = promisifyRequestCall(indexedDB, 'open', [name, version]);
+      var request = p.request;
+
+      request.onupgradeneeded = function(event) {
+        if (upgradeCallback) {
+          upgradeCallback(new UpgradeDB(request.result, event.oldVersion, request.transaction));
+        }
+      };
+
+      return p.then(function(db) {
+        return new DB(db);
+      });
+    },
+    delete: function(name) {
+      return promisifyRequestCall(indexedDB, 'deleteDatabase', [name]);
+    }
+  };
+
+  if (typeof module !== 'undefined') {
+    module.exports = exp;
+  }
+  else {
+    self.idb = exp;
+  }
+}());
+
+
+
+/****************************************************
+ * DBHelper starts here
+ *
  * Common database helper functions.
- */
+ *
+ * IDB functions derived from "wittr/js/idb-test/index.js"
+ * and https://github.com/jakearchibald/idb/blob/master/README.md
+ * Both by Jake Archibald
+ *****************************************************/
+
 class DBHelper {
 
   /**
@@ -8,47 +335,138 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    // const port = 8000 // Change this to your server port
+    // return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
+    // Open or generate db
+    const dbPromise = idb.open('restaurantIDB', 1, function (upgradeDb) {
+      switch (upgradeDb.oldVersion) {
+        case 0:
+          const restaurantStore = upgradeDb.createObjectStore('restaurants', {
+            keyPath: 'id'
+          });
+          const masterStore = upgradeDb.createObjectStore('master');
       }
-    };
-    xhr.send();
-  }
+    })
+    // See if all restaurants have been saved
+    dbPromise.then(function(db) {
+      var tx = db.transaction('master');
+      var keyValStore = tx.objectStore('master');
+      return keyValStore.get('all_restaurants');
+    }).then(function(val) {
+      // console.log('The value of "all_restaurants" is:', val);
+      if (val === undefined) { // Nope => get them
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', DBHelper.DATABASE_URL);
+      xhr.onload = () => {
+        if (xhr.status === 200) { // Got a success response from server!
+          const restaurants = JSON.parse(xhr.responseText);
+
+          dbPromise.then(db => {
+            const tx = db.transaction('restaurants','readwrite');
+            const restaurantStore = tx.objectStore('restaurants');
+            restaurants.forEach(function (restaurant) {
+              restaurantStore.put(restaurant);
+            })
+          });
+          dbPromise.then(db => {
+            const tx = db.transaction('master','readwrite');
+            const masterStore = tx.objectStore('master');
+            masterStore.put("saved", "all_restaurants");
+          });
+
+          callback(null, restaurants);
+        } else { // Oops!. Got an error from server.
+          const error = (`Request failed. Returned status of ${xhr.status}`);
+          callback(error, null);
+        }
+      };
+      xhr.send();
+    } else { // Full set already in DB -> assemble from there
+      dbPromise.then(db => {
+        return db.transaction('restaurants')
+          .objectStore('restaurants').getAll();
+      }).then(allObjs => callback(null, allObjs));
+    }
+  }).catch(function(err) {
+    console.log('all_restaurants not found, returned:', err)
+  });
+
+}
 
   /**
    * Fetch a restaurant by its ID.
    */
   static fetchRestaurantById(id, callback) {
-    // fetch all restaurants with proper error handling.
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        const restaurant = restaurants.find(r => r.id == id);
-        if (restaurant) { // Got the restaurant
-          callback(null, restaurant);
-        } else { // Restaurant does not exist in the database
-          callback('Restaurant does not exist', null);
-        }
+    // Open or generate db
+    let dbPromise = idb.open('restaurantIDB', 1, function (upgradeDb) {
+      switch (upgradeDb.oldVersion) {
+        case 0:
+          const restaurantStore = upgradeDb.createObjectStore('restaurants', {
+            keyPath: 'id'
+          });
+          const masterStore = upgradeDb.createObjectStore('master');
       }
-    });
+    })
+    console.log("DB open, about to fetch: ", id);
+    // Try to get our restaurant from the db:
+    dbPromise.then(db => {
+      return db.transaction('restaurants')
+        .objectStore('restaurants').get(parseInt(id));
+    }).then(obj => {
+      console.log("get id from db returned: ", obj);
+      if (obj === undefined) {
+        // Failed to find restaurant {id} from the db => get it
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', DBHelper.DATABASE_URL + `/${id}`);
+        xhr.onload = () => {
+          if (xhr.status === 200) { // Got a success response from server!
+            const restaurant = JSON.parse(xhr.responseText);
+            // Save it to the db:
+            dbPromise.then(db => {
+              const tx = db.transaction('restaurants','readwrite');
+              const restaurantStore = tx.objectStore('restaurants');
+              restaurantStore.put(restaurant);
+            });
+            callback(null, restaurant);
+          } else { // Oops!. Got an error from server.
+            const error = (`Request failed. Returned status of ${xhr.status}`);
+            callback(error, null);
+          }
+        };
+        xhr.send();
+      } else {
+        callback(null,obj)
+      }
+    })
   }
+
+
+  /**
+   * Fetch a restaurant by its ID.
+   */
+  // static fetchRestaurantById(id, callback) {
+  //   // fetch all restaurants with proper error handling.
+  //   DBHelper.fetchRestaurants((error, restaurants) => {
+  //     if (error) {
+  //       callback(error, null);
+  //     } else {
+  //       const restaurant = restaurants.find(r => r.id == id);
+  //       if (restaurant) { // Got the restaurant
+  //         callback(null, restaurant);
+  //       } else { // Restaurant does not exist in the database
+  //         callback('Restaurant does not exist', null);
+  //       }
+  //     }
+  //   });
+  // }
 
   /**
    * Fetch restaurants by a cuisine type with proper error handling.
@@ -150,14 +568,15 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
   /**
    * Small restaurant image URL.
    */
   static smallImageUrlForRestaurant(restaurant) {
-    let smallFile = restaurant.photograph.slice(0, -4) + '-400w' + '.jpg';
+    // let smallFile = restaurant.photograph.slice(0, -4) + '-400w' + '.jpg';
+    let smallFile = restaurant.photograph + '-400w.jpg';
     return (`/img_converted/${smallFile}`);
   }
 
@@ -166,7 +585,8 @@ class DBHelper {
    * Restaurant image alt text.
    */
   static altTextForRestaurantImage(restaurant) {
-    return (`${restaurant.alt}`);
+    return (`${restaurant.name}`);
+    // return (`${restaurant.alt}`);
   }
 
   /**
